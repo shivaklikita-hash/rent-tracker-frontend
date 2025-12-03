224984a78b7bd848a09910eea19e0131ec427482
shivaklikita-hash <shivaklikita@gmail.com>
2025-12-03 07:14:45 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:10000",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export function setToken(token){
+// helper to set auth header after login
+export function setToken(token) {
   API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 }
 
